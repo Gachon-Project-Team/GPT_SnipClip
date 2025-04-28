06f6cd6d34544ec27ee055e469f8db12cea98c2c
swpark <swp1017@naver.com>
2025-04-28 11:23:31 +0900

    메인 함수 수정

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import generate_script
-import generate_image
 import generate_scrap
-from datetime import datetime
+import generate_script
+import generate_real_image
+import generate_ai_image
 import json
- 
+
 def execute(query):
-  news=generate_scrap.news_scraper(query)
-  with open("scrap.json", "w", encoding="utf-8") as json_file:
-     json.dump(news, json_file, indent=4, ensure_ascii=False)
-  with open("scrap.json", "r", encoding="utf-8") as json_file:
-      news = json.load(json_file) 
-  script=generate_script.generate_script(news, query)
-  with open("script.json", "w", encoding="utf-8") as json_file:
-     json.dump(script, json_file, indent=4, ensure_ascii=False)
-   # with open("script.json", "r", encoding="utf-8") as json_file:
-      # script = json.load(json_file)
-   # result = generate_image.generate_image(script, query)
-   # with open("result.json", "w", encoding="utf-8") as json_file:
-      # json.dump(result, json_file, indent=4, ensure_ascii=False)
-      
-execute("가천대학교") 
+    # 뉴스 스크래핑 및 스크립트 생성
+    news = generate_scrap.news_scraper(query)
+    script = generate_script.generate_script(news, query)
+    
+    # 실제 이미지 버전
+    real_result = generate_real_image.generate_real_image(script, query)
+    with open("result.json", "w", encoding="utf-8") as json_file:
+        json.dump(real_result, json_file, indent=4, ensure_ascii=False)
+
+    '''# AI 이미지 버전
+    ai_result = generate_ai_image.generate_ai_image(script, query)
+    with open("ai_result.json", "w", encoding="utf-8") as json_file:
+        json.dump(ai_result, json_file, indent=4, ensure_ascii=False)'''
+    
+    return {
+        "real_result": real_result,
+        #"ai_result": ai_result,
+    }
+
+if __name__ == "__main__":
+    result = execute("SKT 해킹")
