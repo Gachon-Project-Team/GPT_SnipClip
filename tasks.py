2e7d1b6f9102c32b6694411ada65d13e61d32094
swpark <swp1017@naver.com>
2025-04-28 11:26:14 +0900

    import문 수정

--- tasks.py
+++ tasks.py
@@ -1,11 +1,11 @@
 from celery_worker import celery_app
-from generate_image import generate_image
+from generate_real_image import generate_real_image
 from utils.save_json import save_json_result
 from generate_video import process_files
 
 @celery_app.task
 def run_image_generation(script, query):
-    result = generate_image(script, query)
+    result = generate_real_image(script, query)
     save_json_result(result, query, "image")
     return result
 
